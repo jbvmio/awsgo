3bb6a05d2ac943c7cf6e3916b3b5945d84563d67
Jimmy Bonds <jbonds@jbvm.io>
2020-01-24 12:21:05 -0500

    add const

--- awsDefaults.go
+++ awsDefaults.go
@@ -2,6 +2,9 @@ package awsgo
 
 import "github.com/aws/aws-sdk-go/aws"
 
+// DefaultConfigName holds all defaults if there is not any overrides specifid for a ServiceType.
+const DefaultConfigName = `defaults`
+
 // ConfigOptions contains the options that deliver configuration details.
 type ConfigOptions interface {
 	GetDefaults(ServiceType) *aws.Config
